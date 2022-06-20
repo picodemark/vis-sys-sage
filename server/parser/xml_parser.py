d9f14aec13f04f9ed98be54f12e6588e36e61852
Mark Pirkowski <ge93wut@mytum.de>
2022-06-20 19:34:05 +0200

    Adapt algorithm for creating d3 tree graphs

--- server/parser/xml_parser.py
+++ server/parser/xml_parser.py
@@ -12,6 +12,7 @@ class XMLParser:
     def __init__(self):
         self.xml = None
         self.__load_xml_from_file()
+        self.attributes = ["name", "cache_level", "cache_size"]
 
     def __load_xml_from_file(self):
         with open(os.path.join(__location__, 'test_data/base_example.xml')) as xml_file:
@@ -23,41 +24,53 @@ class XMLParser:
     def get_d3_tree_graph(self):
         converted_dict = copy.deepcopy(self.__get_dict())
         converted_dict = converted_dict['sys-sage']['components']['Topology']
-        dict_to_d3_tree_graph_rec(converted_dict)
+        self.__dict_to_d3_tree_graph_rec(converted_dict)
         return converted_dict
 
+    def get_compound_dict(self):
+        pass
 
-def dict_to_d3_tree_graph_rec(tree_dict):
-    if isinstance(tree_dict, list):
-        for tree_dict_elem in tree_dict:
-            node_dict_to_d3_tree_graph_rec(tree_dict_elem)
-    else:
-        node_dict_to_d3_tree_graph_rec(tree_dict)
+    def __dict_to_d3_tree_graph_rec(self, tree_dict):
+        if isinstance(tree_dict, list):
+            for tree_dict_elem in tree_dict:
+                self.__node_dict_to_d3_tree_graph_rec(tree_dict_elem)
+        else:
+            self.__node_dict_to_d3_tree_graph_rec(tree_dict)
 
+    def __node_dict_to_d3_tree_graph_rec(self, node_dict):
+        # copy for iterating
+        copy_dict = copy.deepcopy(node_dict)
 
-def node_dict_to_d3_tree_graph_rec(node_dict):
-    # copy for iterating
-    copy_dict = copy.deepcopy(node_dict)
+        # adapt tree node according to requirements of d3 tree graph
+        for key, value in copy_dict.items():
+            # set name
+            if key == "@name":
+                node_dict['name'] = value
+            # set attributes and node info
+            elif key[0] == "@":
+                if "attributes" not in node_dict:
+                    node_dict["attributes"] = {}
+                if "info" not in node_dict:
+                    node_dict["info"] = {}
+                if key[1:] in self.attributes:
+                    node_dict["attributes"][key[1:]] = value
+                else:
+                    node_dict["info"][key[1:]] = value
+            # set children
+            elif isinstance(value, dict):
+                if "children" in node_dict:
+                    node_dict["children"] = node_dict["children"] + [value]
+                else:
+                    node_dict["children"] = [value]
+            elif isinstance(value, list):
+                if "children" in node_dict:
+                    node_dict["children"] = node_dict["children"] + value
+                else:
+                    node_dict["children"] = value
+            # delete key
+            del node_dict[key]
 
-    # adapt tree node according to requirements of d3 tree graph
-    for key, value in copy_dict.items():
-        # set name
-        if key == "@name":
-            node_dict['name'] = value
-        # set attributes
-        elif key[0] == "@":
-            if "attributes" not in node_dict:
-                node_dict["attributes"] = {}
-            node_dict["attributes"][key[1:]] = value
-        # set children
-        elif isinstance(value, dict):
-            node_dict["children"] = [value]
-        elif isinstance(value, list):
-            node_dict["children"] = value
-        # delete key
-        del node_dict[key]
-
-    # call recursive function for all children nodes
-    if 'children' in node_dict:
-        for child in node_dict["children"]:
-            dict_to_d3_tree_graph_rec(child)
+        # call recursive function for all children nodes
+        if "children" in node_dict:
+            for child in node_dict["children"]:
+                self.__dict_to_d3_tree_graph_rec(child)
