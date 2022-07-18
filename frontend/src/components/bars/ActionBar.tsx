8c67ba6222d43f315a2f046634f03cdb839a4cf4
Mark Pirkowski <ge93wut@mytum.de>
2022-07-18 12:32:12 +0200

    Adjust positioning of components in Toolbar component

--- frontend/src/components/bars/ActionBar.tsx
+++ frontend/src/components/bars/ActionBar.tsx
@@ -5,24 +5,23 @@ import Toolbar from '@mui/material/Toolbar';
 import { ButtonGroup, Grid, Input } from '@mui/material';
 import Button from '@mui/material/Button';
 import UploadButton from '../buttons/UploadButton';
+import NodeList from '../select/NodeList';
 
 export default function ActionBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color={'secondary'}>
-        <Toolbar variant="dense">
-          <Grid justifyContent="flex-end" container spacing={24}>
-            <Grid item justifyContent="flex-end">
-              <ButtonGroup
-                size="small"
-                variant="contained"
-                aria-label="small secondary button group">
-                <UploadButton></UploadButton>
-                <Button>Save</Button>
-                <Button>Export</Button>
-              </ButtonGroup>
-            </Grid>
-          </Grid>
+        <Toolbar variant="dense" sx={{ justifyContent: 'space-between' }}>
+          <Box>
+            <NodeList />
+          </Box>
+          <Box>
+            <ButtonGroup size="small" variant="contained" aria-label="small secondary button group">
+              <UploadButton></UploadButton>
+              <Button>Save</Button>
+              <Button>Export</Button>
+            </ButtonGroup>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
