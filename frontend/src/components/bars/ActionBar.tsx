edbd38b900974bae2f07915aa75eafef4cf05c4f
Mark Pirkowski <ge93wut@mytum.de>
2022-09-10 15:46:57 +0200

    Adjust positions of bars in the header

--- frontend/src/components/bars/ActionBar.tsx
+++ frontend/src/components/bars/ActionBar.tsx
@@ -4,26 +4,24 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import { ButtonGroup } from '@mui/material';
 import Button from '@mui/material/Button';
-import UploadButton from '../buttons/UploadButton';
+import LoadButton from '../buttons/LoadButton';
 import NodeList from '../select/NodeList';
 
 export default function ActionBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color={'secondary'}>
-        <Toolbar variant="dense" sx={{ justifyContent: 'space-between' }}>
-          <Box>
-            <NodeList />
-          </Box>
-          <Box>
-            <ButtonGroup size="small" variant="contained" aria-label="small secondary button group">
-              <UploadButton></UploadButton>
-              <Button>Save</Button>
-              <Button>Export</Button>
-            </ButtonGroup>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky" color="inherit" elevation={1}>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Box>
+          <NodeList />
+        </Box>
+        <Box>
+          <ButtonGroup size="small" variant="contained" aria-label="small secondary button group">
+            <LoadButton></LoadButton>
+            <Button disabled>Save</Button>
+            <Button disabled>Export</Button>
+          </ButtonGroup>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
--- frontend/src/components/bars/NavBar.tsx
+++ frontend/src/components/bars/NavBar.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import GitHubButton from '../buttons/GitHubButton';
 
 export default function NavBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
-            Vis-Sys-Sage
-          </Typography>
-          <GitHubButton />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="relative">
+      <Toolbar>
+        <Typography
+          variant="h5"
+          color="inherit"
+          fontWeight="bold"
+          component="div"
+          sx={{ flexGrow: 1 }}>
+          Vis-Sys-Sage
+        </Typography>
+        <GitHubButton />
+      </Toolbar>
+    </AppBar>
   );
 }
