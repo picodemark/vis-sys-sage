c43d99881984a84d940916cc2b292db43593b2dd
Mark Pirkowski <ge93wut@mytum.de>
2022-09-12 02:51:17 +0200

    Make graph responsive

--- frontend/src/components/graphs/component-tree/TreeGraph.tsx
+++ frontend/src/components/graphs/component-tree/TreeGraph.tsx
@@ -1,47 +1,49 @@
-import { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Tree from 'react-d3-tree';
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { Toolbar, Typography } from '@mui/material';
-import TreeNode from './TreeNode';
+import TreeComponent from './TreeComponent';
 import { selectCurrentTree } from '../../../store/graphDataSlice';
-
-const containerStyles = {
-  width: '100%',
-  height: '50rem'
-};
+import Box from '@mui/material/Box';
 
 export default function TreeGraph() {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   const selector = useSelector((state) => selectCurrentTree(state));
+
+  const box = useRef(null);
 
   const [translate, setTranslate] = useState({
     x: 0,
     y: 0
   });
 
-  const treeGraphRef = useRef();
+  const setPosition = () => {
+    setTranslate({ x: box.current.offsetWidth / 2, y: box.current.offsetHeight / 10 });
+  };
+
+  useEffect(() => {
+    setPosition();
+    window.addEventListener('resize', setPosition);
+  }, []);
 
   return (
     <React.Fragment>
-      <Toolbar variant="dense" color="primary">
-        <Typography variant="h6" color="inherit" component="div">
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="primary" component="div">
           Tree Config
         </Typography>
       </Toolbar>
-      <div style={containerStyles} ref={treeGraphRef}>
+      <Box sx={{ width: '100%', height: '600px' }} ref={box}>
         <Tree
           data={selector}
           pathFunc={'step'}
           translate={translate}
           orientation={'vertical'}
-          renderCustomNodeElement={TreeNode}
-          depthFactor={500}
+          renderCustomNodeElement={TreeComponent}
+          depthFactor={450}
           initialDepth={1}
-          separation={{ siblings: 5, nonSiblings: 5 }}
+          separation={{ siblings: 3, nonSiblings: 3 }}
         />
-      </div>
+      </Box>
     </React.Fragment>
   );
 }
