c7a6908e8c04187bf3264fc122c8086aa782ef88
Mark Pirkowski <ge93wut@mytum.de>
2022-09-12 02:50:24 +0200

    Add data path component

---
+++ frontend/src/components/graphs/data-paths/DataGraphComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ComponentAvatar from '../ComponentAvatar';
+
+export default function DataGraphComponent(props: any) {
+  const { id } = props;
+
+  return <ComponentAvatar id={id} />;
+}
