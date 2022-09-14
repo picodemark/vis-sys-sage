2102187ad40c8b28ae615763df8179fd3b4f5fb6
Mark Pirkowski <ge93wut@mytum.de>
2022-09-15 01:31:39 +0200

    Add table for all components

--- frontend/src/components/bars/ActionBar.tsx
+++ frontend/src/components/bars/ActionBar.tsx
@@ -4,13 +4,15 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import UploadButton from '../buttons/UploadButton';
 import NodeList from '../select/NodeList';
+import ComponentsButton from '../buttons/ComponentsButton';
 
 export default function ActionBar() {
   return (
     <AppBar position="sticky" color="inherit" elevation={1}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Box>
+        <Box display="flex" alignItems="center">
           <NodeList />
+          <ComponentsButton />
         </Box>
         <Box>
           <UploadButton />
