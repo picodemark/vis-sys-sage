5ec564f0dfdb6ff7dbf0803e3d539e4e1db4261a
Mark Pirkowski <ge93wut@mytum.de>
2022-09-19 19:34:08 +0200

    Remove unused reporting of web vitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import App from './app/App';
-import { StyledEngineProvider } from '@mui/material/styles';
+import App from './components/app/App';
 
 // CSS stylesheets
 import './style/general.css';
@@ -14,13 +12,4 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <StyledEngineProvider injectFirst>
-    <App />
-  </StyledEngineProvider>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+root.render(<App />);
