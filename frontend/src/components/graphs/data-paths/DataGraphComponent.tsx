630338848a7c4ef2ec82b81b24d59f91a458de53
Mark Pirkowski <ge93wut@mytum.de>
2022-09-19 19:54:16 +0200

    Replace data path graph component with extensive version

--- frontend/src/components/graphs/data-paths/DataGraphComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ComponentAvatar from '../ComponentAvatar';
-
-export default function DataGraphComponent(props: any) {
-  const { id } = props;
-
-  return <ComponentAvatar id={id} clickable={true} />;
-}
---
+++ frontend/src/components/graphs/data-paths/DataPathComponent.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import ComponentAvatar from '../ComponentAvatar';
+import { useSelector } from 'react-redux';
+import { selectClickedComponent, setClickedComponent } from '../../../store/graphDataSlice';
+import { useAppDispatch } from '../../../hooks/hooks';
+import { Attributes } from '../../../types/types';
+
+interface Props {
+  id: string;
+  info: Record<string, string | Attributes>;
+}
+
+function DataPathComponent(props: Props) {
+  const { id, info } = props;
+
+  const clickedComponentSelector = useSelector((state) => selectClickedComponent(state) === id);
+  const dispatch = useAppDispatch();
+
+  const [highlight, setHighlight] = useState(false);
+
+  const triggerHighlight = () => {
+    dispatch(setClickedComponent(id));
+  };
+
+  // highlight only when component ID is the same
+  if (!highlight && clickedComponentSelector) {
+    setHighlight(true);
+  }
+
+  // remove highlight when component ID is the same and component is highlighted
+  if (highlight && !clickedComponentSelector) {
+    setHighlight(false);
+  }
+
+  return (
+    <div
+      style={{
+        width: '110px',
+        borderRadius: '5px',
+        backgroundColor: highlight ? 'yellow' : '#dad7cd',
+        fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+        display: 'flex',
+        cursor: 'pointer'
+      }}
+      onClick={triggerHighlight}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '0.5rem',
+          fontSize: '30px'
+        }}>
+        <ComponentAvatar label={info.name as string} attributes={info.attributes as Attributes} />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '0.5rem 0.5rem 0.5rem 0',
+          textAlign: 'center'
+        }}>
+        <span style={{ fontSize: '30px', fontWeight: 800, color: '#0065bd' }}>
+          {info.nodeID as string}
+        </span>
+        <span style={{ fontSize: '24px', marginTop: 'auto' }}>{info.componentID as string}</span>
+      </div>
+    </div>
+  );
+}
+
+export const MemoDataGraphComponent = React.memo(DataPathComponent);
