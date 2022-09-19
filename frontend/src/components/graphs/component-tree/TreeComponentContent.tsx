eeeb63ce9cee0075172ac012ee2de332fe5a8a55
Mark Pirkowski <ge93wut@mytum.de>
2022-09-19 19:39:04 +0200

    Extract component content to enable state usage

---
+++ frontend/src/components/graphs/component-tree/TreeComponentContent.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { CustomNodeElementProps, TreeNodeDatum } from 'react-d3-tree/lib/types/common';
+import ComponentAvatar from '../ComponentAvatar';
+import TreeComponentArrow from './TreeComponentArrow';
+import { useSelector } from 'react-redux';
+import { selectClickedComponent } from '../../../store/graphDataSlice';
+import { useAppDispatch } from '../../../hooks/hooks';
+
+import { setClickedComponent } from '../../../store/graphDataSlice';
+
+interface CustomTreeNodeDatum extends TreeNodeDatum {
+  id?: string;
+  uniqueComponentID?: string;
+}
+
+// replace nodeDatum with custom node nodeDatum
+interface AdaptedCustomNodeElementProps extends Omit<CustomNodeElementProps, 'nodeDatum'> {
+  nodeDatum: CustomTreeNodeDatum;
+}
+
+export default function TreeComponentContent(props: AdaptedCustomNodeElementProps) {
+  const { nodeDatum, toggleNode } = props;
+
+  const dispatch = useAppDispatch();
+
+  const clickedComponentSelector = useSelector(
+    (state) => selectClickedComponent(state) === nodeDatum.uniqueComponentID
+  );
+
+  const [highlight, setHighlight] = useState(false);
+
+  const triggerHighlight = () => {
+    dispatch(setClickedComponent(nodeDatum.uniqueComponentID));
+  };
+
+  // highlight only when component ID is the same
+  if (!highlight && clickedComponentSelector) {
+    setHighlight(true);
+  }
+
+  // remove highlight when component ID is the same and component is highlighted
+  if (highlight && !clickedComponentSelector) {
+    setHighlight(false);
+  }
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '250px',
+          borderRadius: '10px',
+          backgroundColor: highlight ? 'yellow' : '#dad7cd',
+          fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+          cursor: 'pointer'
+        }}
+        onClick={triggerHighlight}>
+        <div style={{ display: 'flex', alignItems: 'center', padding: '1rem', fontSize: '30px' }}>
+          <ComponentAvatar label={nodeDatum.name} attributes={nodeDatum?.attributes} />
+          <span style={{ marginLeft: '0.5rem' }}>
+            {nodeDatum.name === '' ? 'no name' : nodeDatum.name}
+          </span>
+        </div>
+        <div style={{ fontSize: '26px', padding: '1rem' }}>
+          <span>{nodeDatum?.id === undefined ? 'No ID' : 'ID: ' + nodeDatum.id}</span>
+          {nodeDatum.attributes && (
+            <span style={{ float: 'right' }}>INFO: {Object.keys(nodeDatum.attributes).length}</span>
+          )}
+        </div>
+        {nodeDatum.children && (
+          <div
+            style={{
+              alignSelf: 'flex-end',
+              marginTop: 'auto',
+              padding: '1rem',
+              cursor: 'pointer'
+            }}
+            onClick={toggleNode}>
+            <TreeComponentArrow direction={nodeDatum.__rd3t.collapsed ? 'up' : 'down'} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
