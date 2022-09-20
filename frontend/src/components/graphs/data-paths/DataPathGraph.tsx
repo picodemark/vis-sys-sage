5bdb245f7fdbeb9eb3be2c263cfa6e17d3db1198
Mark Pirkowski <ge93wut@mytum.de>
2022-09-21 01:55:45 +0200

    Introduce tabs to switch between both tables in dialog

--- frontend/src/components/graphs/data-paths/DataPathGraph.tsx
+++ frontend/src/components/graphs/data-paths/DataPathGraph.tsx
@@ -3,26 +3,36 @@ import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import { MemoDataGraphComponent } from './DataPathComponent';
-import { useAppSelector } from '../../../hooks/hooks';
-import { selectFilteredDataPath } from '../../../store/graphDataSlice';
+import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
+import { selectFilteredDataPath, setClickedComponents } from '../../../store/graphDataSlice';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DataPathTable from '../../tables/DataPathTable';
+import NewDataPathTable from '../../tables/NewDataPathTable';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 export default function DataPathGraph() {
   const filteredGraphDataSelector = useAppSelector((state) => selectFilteredDataPath(state));
+
+  const dispatch = useAppDispatch();
 
   const [extraConfig, setExtraConfig] = useState({});
 
   const [keyCount, setKeyCount] = useState<number>(0);
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState<boolean>(false);
 
-  const [link, setLink] = useState({
+  const [link, setLink] = useState<Record<string, string>>({
     source: '',
     target: ''
   });
+
+  const [value, setValue] = React.useState<number>(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   const openDialog = () => {
     setOpen(true);
@@ -54,6 +64,7 @@ export default function DataPathGraph() {
   useMemo(() => forceReMount(), [filteredGraphDataSelector]);
 
   const onClickLink = (source: string, target: string) => {
+    dispatch(setClickedComponents([source, target]));
     setLink({
       source: source,
       target: target
@@ -67,10 +78,10 @@ export default function DataPathGraph() {
     nodeHighlightBehavior: true, // highlight all nodes on mouse-over
     linkHighlightBehavior: true, // highlight all nodes on mouse-over
     d3: {
-      gravity: -1000,
-      linkLength: 300,
+      gravity: -2300,
+      linkLength: 350,
       linkStrength: 1,
-      alphaTarget: 0.05,
+      alphaTarget: 0.1,
       disableLinkForce: false
     },
     node: {
@@ -95,7 +106,7 @@ export default function DataPathGraph() {
           id="data-path-graph" // ID of data-path graph wrapper
           data={filteredGraphDataSelector} // data
           config={config} // config
-          onClickLink={onClickLink}
+          onClickLink={onClickLink} // call function on link click
         />
       </Box>
       <Dialog open={open} onClose={closeDialog} maxWidth="xl">
@@ -103,12 +114,18 @@ export default function DataPathGraph() {
           {'Data-Paths between :' + link.source + ' and ' + link.target}{' '}
         </DialogTitle>
         <DialogContent sx={{ padding: 0 }}>
-          <Box display={'flex'} width={'100%'} minHeight={400}>
-            <Box width={500} sx={{ borderRight: 1, borderColor: 'grey.500' }}>
-              <DataPathTable source={link.source} target={link.target} />
+          <Box display={'flex'} flexDirection={'column'} width={'100%'} minHeight={500}>
+            <Box sx={{ width: '100%' }}>
+              <Tabs value={value} onChange={handleChange} centered>
+                <Tab label="From To" />
+                <Tab label="From To" />
+              </Tabs>
             </Box>
-            <Box width={500}>
-              <DataPathTable source={link.target} target={link.source} />
+            <Box hidden={value !== 0} width={1000}>
+              {value === 0 && <NewDataPathTable source={link.source} target={link.target} />}
+            </Box>
+            <Box hidden={value !== 1} width={1000}>
+              {value === 1 && <NewDataPathTable source={link.target} target={link.source} />}
             </Box>
           </Box>
         </DialogContent>
