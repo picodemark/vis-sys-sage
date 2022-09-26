8adac2aa6c36ad4ba9c8120088199251003c20e4
Mark Pirkowski <ge93wut@mytum.de>
2022-09-26 13:50:59 +0200

    Add dictionary with info on all components Refactor according to renamed variables and functions Use reselect for tree filtering

--- frontend/src/store/graphDataSlice.tsx
+++ frontend/src/store/graphDataSlice.tsx
@@ -1,16 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
 import { RawNodeDatum } from 'react-d3-tree/lib/types/common';
-import { ComponentListItem, NodeListItem } from '../types/types';
+import { ComponentListItem, CustomRawNodeDatum, NodeListItem } from '../types/types';
 
 interface Props {
   nodeID: string;
   tree: RawNodeDatum;
   componentsList: ComponentListItem[];
+  componentsInfo: Record<string, ComponentListItem>;
   nodesList: NodeListItem[];
   dataPath: any;
   dataPathLinkAttributes: any;
-  clickedComponents: string[];
+  highlightedComponents: string[];
 }
 
 const initialState: Props = {
@@ -19,13 +20,14 @@ const initialState: Props = {
     name: ''
   },
   componentsList: [],
+  componentsInfo: {},
   nodesList: [],
   dataPath: {
     nodes: [],
     links: []
   },
   dataPathLinkAttributes: [],
-  clickedComponents: []
+  highlightedComponents: []
 };
 
 export const graphDataSlice = createSlice({
@@ -36,78 +38,76 @@ export const graphDataSlice = createSlice({
       // set state based on payload
       state.tree = action.payload?.tree;
       state.componentsList = action.payload?.componentsList;
+      state.componentsInfo = action.payload?.componentsInfo;
       state.nodesList = action.payload?.nodesList;
       state.dataPath.nodes = action.payload?.dataPath?.nodes;
       state.dataPath.links = action.payload?.dataPath?.links;
       state.dataPathLinkAttributes = action.payload?.dataPath?.attributes;
 
-      // show all nodes
+      // show all nodes as default
       state.nodeID = 'all';
 
-      // no component selected
-      state.clickedComponents = [];
+      // no components highlighted as default
+      state.highlightedComponents = [];
     },
     setNodeID: (state, action: PayloadAction<string>) => {
       state.nodeID = action.payload;
     },
-    setClickedComponents: (state, action: PayloadAction<string[]>) => {
-      state.clickedComponents = action.payload;
+    setHighlightedComponents: (state, action: PayloadAction<string[]>) => {
+      state.highlightedComponents = action.payload;
     }
   }
 });
 
-export const { setGraphData, setNodeID, setClickedComponents } = graphDataSlice.actions;
+export const { setGraphData, setNodeID, setHighlightedComponents } = graphDataSlice.actions;
 
 export default graphDataSlice.reducer;
 
-const selectNodeID = (state) => state.graphData.nodeID;
+export const selectNodeID = (state) => state.graphData.nodeID;
 
-export const selectCurrentTree = (state) => {
-  if (state.graphData.nodeID === 'all') {
-    return state.graphData.tree;
+export const selectCurrentTree = createSelector(
+  selectNodeID,
+  (state) => state.graphData.tree,
+  (nodeID: string, tree: CustomRawNodeDatum) => {
+    if (nodeID === 'all') {
+      return tree;
+    }
+    return tree.children.filter((node) => node.id === nodeID)[0];
   }
-  const filteredTree = state.graphData.tree.children.filter(
-    (node) => node.id === state.graphData.nodeID
-  );
-  return filteredTree[0];
-};
+);
 
 export const selectComponentsList = (state) => state.graphData.componentsList;
 
-export const selectNodeList = (state) => state.graphData.nodesList;
+export const selectComponentsInfo = (state) => state.graphData.componentsInfo;
 
-const selectDataPath = (state) => state.graphData.dataPath;
+export const selectNodeList = (state) => state.graphData.nodesList;
 
 export const selectFilteredDataPath = createSelector(
   selectNodeID,
-  selectDataPath,
+  (state) => state.graphData.dataPath,
   (nodeID, dataPath) => {
-    const dataPathNew = {
-      nodes: [],
-      links: []
-    };
+    const dataPathNew = {};
 
     if (nodeID === 'all') {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      dataPathNew.nodes = Object.values(dataPath.nodes).reduce(
+      // merge all components per node into single list
+      dataPathNew['nodes'] = Object.values(dataPath.nodes).reduce(
         (prev: any, current: any) => prev.concat(current),
         []
       );
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      dataPathNew.links = Object.values(dataPath.links).reduce(
+      // merge all links per node into single list
+      dataPathNew['links'] = Object.values(dataPath.links).reduce(
         (prev: any, current: any) => prev.concat(current),
         []
       );
     } else {
-      dataPathNew.nodes = dataPath.nodes[nodeID];
-      dataPathNew.links = dataPath.links[nodeID];
+      dataPathNew['nodes'] = dataPath.nodes[nodeID];
+      dataPathNew['links'] = dataPath.links[nodeID];
     }
+
     return dataPathNew;
   }
 );
 
 export const selectDataPathLinkAttributes = (state) => state.graphData.dataPathLinkAttributes;
 
-export const selectClickedComponents = (state) => state.graphData.clickedComponents;
+export const selectHighlightedComponents = (state) => state.graphData.highlightedComponents;
