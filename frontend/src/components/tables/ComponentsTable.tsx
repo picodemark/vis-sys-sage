1a5a18d59444075c1812eb548ba2015ff90df7b1
Mark Pirkowski <ge93wut@mytum.de>
2022-09-26 13:34:45 +0200

    Replace datagrid by MUI datatable

--- frontend/src/components/tables/ComponentsTable.tsx
+++ frontend/src/components/tables/ComponentsTable.tsx
@@ -1,60 +1,114 @@
-import * as React from 'react';
-import { DataGrid, GridCellParams, GridColDef, GridToolbarQuickFilter } from '@mui/x-data-grid';
-import Box from '@mui/material/Box';
+import React from 'react';
+import MUIDataTable from 'mui-datatables';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import ComponentAvatar from '../graphs/ComponentAvatar';
-import Typography from '@mui/material/Typography';
+import HighlightComponentsButton from '../buttons/HighlightComponentsButton';
+import { StyledTableCell } from './util/common';
 
-const columns: GridColDef[] = [
-  {
-    field: 'unique_component_id',
-    headerName: 'Symbol',
-    renderCell: (params: GridCellParams) => <ComponentAvatar id={params.value} clickable={true} />,
-    width: 80,
-    align: 'center'
-  },
-  { field: 'name', headerName: 'Name', headerClassName: 'datagrid--header', flex: 1 },
-  { field: 'node_id', headerName: 'Node ID', headerClassName: 'datagrid--header', flex: 1 },
-  {
-    field: 'component_id',
-    headerName: 'Component ID',
-    headerClassName: 'datagrid--header',
-    flex: 1
-  },
-  {
-    field: 'attributes',
-    headerName: 'Attributes',
-    renderCell: (params: GridCellParams) => (
-      <Typography>{params.value && Object.keys(params.value).length}</Typography>
-    ),
-    flex: 1
-  }
-];
-function QuickSearchToolbar() {
-  return (
-    <Box
-      sx={{
-        p: 0.5,
-        pb: 0
-      }}>
-      <GridToolbarQuickFilter />
-    </Box>
-  );
+interface Props {
+  data: any;
 }
 
-export default function ComponentsTable(rowData) {
-  return (
-    <Box sx={{ height: 493, width: 800 }}>
-      <DataGrid
-        disableColumnMenu
-        rows={rowData.rowData}
-        getRowId={(row) => row.unique_component_id}
-        columns={columns}
-        components={{ Toolbar: QuickSearchToolbar }}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        density="compact"
-        sx={{ padding: 0 }}
-      />
-    </Box>
-  );
+export default function ComponentsTable(props: Props) {
+  const { data } = props;
+
+  const columns = [
+    {
+      name: 'name',
+      label: 'Avatar',
+      options: {
+        customBodyRender: (value, tableMeta) =>
+          value === 'cache' ? (
+            <ComponentAvatar
+              label={value}
+              size="small"
+              cacheLevel={
+                tableMeta.tableData[tableMeta.rowIndex].attributes.filter(
+                  (attribute) => attribute.name === 'cache_level'
+                )[0].value
+              }
+            />
+          ) : (
+            <ComponentAvatar label={value} size="small" />
+          ),
+        filter: false,
+        sort: false
+      }
+    },
+    {
+      name: 'name',
+      label: 'Name'
+    },
+    {
+      name: 'nodeID',
+      label: 'Node ID'
+    },
+    {
+      name: 'componentID',
+      label: 'Components ID'
+    },
+    {
+      name: 'attributesNumber',
+      label: 'Attributes'
+    },
+    {
+      name: 'uniqueComponentID',
+      label: 'More',
+      options: {
+        customBodyRender: (value) => (
+          <HighlightComponentsButton label="Show" components={[value]} />
+        ),
+        filter: false,
+        sort: false
+      }
+    }
+  ];
+
+  const options = {
+    elevation: 0,
+    empty: true,
+    download: false,
+    print: false,
+    filter: false,
+    selectableRowsHideCheckboxes: true,
+    rowsPerPage: 10,
+    expandableRowsHeader: false,
+    expandableRows: true,
+    isRowExpandable: (rowIndex) => data[rowIndex]?.attributes !== undefined,
+    renderExpandableRow: (row, meta) => {
+      return (
+        <React.Fragment>
+          {data[meta.rowIndex].attributes && (
+            <tr>
+              <td colSpan={7}>
+                <TableContainer>
+                  <Table style={{ width: '100%' }}>
+                    <TableHead>
+                      <TableRow>
+                        <StyledTableCell>Name</StyledTableCell>
+                        <StyledTableCell>Value</StyledTableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {data[meta.rowIndex].attributes.map((row, index) => (
+                        <TableRow key={index}>
+                          <StyledTableCell>{row.name}</StyledTableCell>
+                          <StyledTableCell>{row.value}</StyledTableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </td>
+            </tr>
+          )}
+        </React.Fragment>
+      );
+    }
+  };
+  return <MUIDataTable data={data} columns={columns} options={options} />;
 }
