f8bca8781bdd04ea0d0b28ea97c6d98fd068253c
Mark Pirkowski <ge93wut@mytum.de>
2022-09-26 13:44:46 +0200

    Add default properties

--- frontend/src/components/DraggablePaperComponent.tsx
+++ frontend/src/components/DraggablePaperComponent.tsx
@@ -3,9 +3,16 @@ import Paper, { PaperProps } from '@mui/material/Paper';
 import Draggable from 'react-draggable';
 
 export default function DraggablePaperComponent(props: PaperProps) {
+  const defaultProps: PaperProps = {
+    elevation: 10
+  };
+
+  // replace passed props by default props
+  const mergedProps = { ...props, ...defaultProps };
+
   return (
     <Draggable cancel={'[class*="MuiDialogContent-root"]'}>
-      <Paper {...props} />
+      <Paper {...mergedProps} />
     </Draggable>
   );
 }
