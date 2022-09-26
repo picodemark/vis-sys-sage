a2e35af72290f6190cc281916e433b9523a283b6
Mark Pirkowski <ge93wut@mytum.de>
2022-09-26 13:33:44 +0200

    Add more commonly used types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,12 +1,29 @@
+import { RawNodeDatum, TreeNodeDatum } from 'react-d3-tree/lib/types/common';
+
 export type Attributes = Record<string, string | number | boolean>;
 
+// type for node list item
 export interface NodeListItem {
   id: string;
 }
 
+// type for components list item
 export interface ComponentListItem {
   nodeID: string;
   uniqueComponentID: string;
   name: string;
   attributes?: Record<string, string | number | boolean>;
+}
+
+// custom type for raw node datum
+export interface CustomRawNodeDatum extends RawNodeDatum {
+  id?: string;
+  uniqueComponentID?: string;
+  children?: CustomRawNodeDatum[];
+}
+
+// custom type for tree node datum
+export interface CustomTreeNodeDatum extends TreeNodeDatum {
+  id?: string;
+  uniqueComponentID?: string;
 }
