1ce5d4f4e103f5651dc56d3d811fd4c74cdadb30
Mark Pirkowski <ge93wut@mytum.de>
2022-10-08 13:48:20 +0200

    Remove strict fixation of package versions

--- server/run.py
+++ server/run.py
@@ -5,6 +5,8 @@ import webbrowser
 from pathlib import Path
 from time import sleep
 
+LOCALHOST = "http://127.0.0.1:5000"
+
 if sys.version_info < (3, 8, 0):
     print("This script assumes at least python 3.8!")
     sys.exit(1)
@@ -32,8 +34,9 @@ def create_venv():
 
 
 def install_packages():
-    print("Install PyPI packages...")
+    print("Upgrade pip...")
     run([get_python_executable(), "-m", "pip", "install", "--upgrade", "pip"])
+    print("Install PyPI packages from requirements.txt...")
     run([get_python_executable(), "-m", "pip", "install", "-r", "requirements.txt"])
 
 
@@ -53,8 +56,8 @@ def run_server():
 
 
 def open_localhost_in_browser():
-    print("Open application at http://127.0.0.1:5000...")
-    webbrowser.open("http://127.0.0.1:5000", new=2)
+    print(f"Open application at {LOCALHOST}...")
+    webbrowser.open(LOCALHOST, new=2)
 
 
 def check_existing_folder(folder):
