17084533bd273115d6a4b8db6847bb8f33fc7b75
Mark Pirkowski <ge93wut@mytum.de>
2023-01-20 21:23:23 +0100

    Change arrow for tree component to solve rendering problem in Safari browser

--- frontend/src/components/graphs/component-tree/TreeComponentArrow.tsx
+++ frontend/src/components/graphs/component-tree/TreeComponentArrow.tsx
@@ -7,15 +7,20 @@ interface Props {
 export default function TreeComponentArrow(props: Props) {
   const { direction } = props;
 
+  const solidTransparentBorder = '10px solid transparent';
+  const solidBlackBorder = '10px solid black';
+
   return (
-    <span
+    <div
       style={{
-        display: 'inline-block',
-        width: '12px',
-        height: '12px',
-        borderTop: '2px solid #000',
-        borderRight: '2px solid #000',
-        transform: direction === 'up' ? 'rotate(-45deg)' : 'rotate(135deg)'
-      }}></span>
+        width: '0px',
+        height: '0px',
+        margin: 'auto',
+        borderLeft: solidTransparentBorder,
+        borderTop: direction === 'down' ? solidBlackBorder : 'unset',
+        borderRight: solidTransparentBorder,
+        borderBottom: direction === 'up' ? solidBlackBorder : 'unset'
+      }}
+    />
   );
 }
